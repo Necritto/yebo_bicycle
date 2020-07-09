cdff85a3e0f6a7ef189a22d4484f479930a6a806
Necritto <onetopyr@mail.ru>
2020-07-09 20:18:28 +0300

    Feature: header adaptive [ref:no-ref]

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,20 @@
 "use strict";
+
+function bgimg() {
+  const bgimg = document.querySelectorAll(".bgimg");
+  for (let i = 0; i < bgimg.length; i++) {
+    if (bgimg[i].querySelector("img")) {
+      bgimg[i].style.backgroundImage =
+        "url(" + bgimg[i].querySelector("img").getAttribute("src") + ")";
+    }
+  }
+}
+
+bgimg();
+
+const menu = document.querySelector(".icon-menu");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  menu.nextElementSibling.classList.toggle("active");
+});
