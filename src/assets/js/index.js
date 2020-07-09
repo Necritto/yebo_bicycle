a66e693d041324b69f65f7919c2701e1a3ebd3ae
Necritto <onetopyr@mail.ru>
2020-07-10 02:16:15 +0300

    Fix: fix scroll && redirect [ref:no-ref]

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// Images through background
+
 function bgimg() {
   const bgimg = document.querySelectorAll(".bgimg");
   for (let i = 0; i < bgimg.length; i++) {
@@ -12,9 +14,36 @@ function bgimg() {
 
 bgimg();
 
+// Burger menu
+
 const menu = document.querySelector(".icon-menu");
 
 menu.addEventListener("click", () => {
   menu.classList.toggle("active");
   menu.nextElementSibling.classList.toggle("active");
 });
+
+// Prevenent default
+
+document.querySelectorAll("a").forEach((el) => {
+  if (el.getAttribute("href") === "#") {
+    el.addEventListener("click", (e) => e.preventDefault());
+  }
+});
+
+// Scroll
+
+const anchors = document.querySelectorAll(".scroll");
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const id = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(id).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
